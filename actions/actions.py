3e8ffd3b9198711a46563cec663458877c55a673
Nikki Satmaka <nsatmaka@gmail.com>
2022-07-04 12:10:54 +0700

    invest in progress

--- actions/actions.py
+++ actions/actions.py
@@ -22,6 +22,60 @@ from rasa_sdk.events import (
     UserUtteranceReverted,
 )
 
+account_balance = 1_000_000
+
+projects_list = {
+    "project_1": {
+        "name": "REG506-AMD001",
+        "total_pinjaman": 100_000_000,
+        "slot_pendanaan": 57_500_000,
+        "sisa_waktu_pendananaan": "2 hari lagi",
+        "grade": "A",
+        "rate": "14%",
+        "durasi_pinjaman": "4 bulan",
+        "tujuan": "invoice financing",
+    },
+    "project_2": {
+        "name": "REG507-NVD005",
+        "total_pinjaman": 100_000_000,
+        "slot_pendanaan": 71_700_000,
+        "sisa_waktu_pendananaan": "1 hari lagi",
+        "grade": "C",
+        "rate": "18%",
+        "durasi_pinjaman": "4 bulan",
+        "tujuan": "invoice financing",
+    },
+    "project_3": {
+        "name": "REG508-INT023",
+        "total_pinjaman": 70_000_000,
+        "slot_pendanaan": 38_500_000,
+        "sisa_waktu_pendananaan": "3 hari lagi",
+        "grade": "C",
+        "rate": "18%",
+        "durasi_pinjaman": "3 bulan",
+        "tujuan": "working capital financing",
+    },
+}
+
+
+class ActionPayProject(Action):
+    """Transfer to invest in project."""
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+        return "action_pay_project"
+
+    async def run(
+            self, dispatcher: CollectingDispatcher,
+            tracker: Tracker, domain: Dict
+        ) -> List[EventType]:
+        """Executes the custom action"""
+
+        # slots = {
+        #     "AA_CONTINUE_FORM": None,
+        #     "zz_confirm_form": None,
+        #     "PROJECT": None,
+        # }
+        pass
 
 class ActionShowBalance(Action):
     """Shows the balance of account"""
@@ -31,12 +85,13 @@ class ActionShowBalance(Action):
         return "action_show_balance"
 
     async def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-    ) -> List[EventType]:
+            self, dispatcher: CollectingDispatcher,
+            tracker: Tracker, domain: Dict
+        ) -> List[EventType]:
         """Executes the custom action"""
 
         # show bank account balance
-        account_balance = random.randint(1, 1000) * 50000
+        account_balance = account_balance
         # amount = tracker.get_slot("amount_transferred")
         dispatcher.utter_message(
             response="utter_account_balance",
