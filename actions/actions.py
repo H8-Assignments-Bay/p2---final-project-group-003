3f021d8e41f4ab38f538fcbea606327edf608833
Nikki Satmaka <nsatmaka@gmail.com>
2022-07-05 00:19:23 +0700

    withdrawal feature mostly done

--- actions/actions.py
+++ actions/actions.py
@@ -10,8 +10,9 @@
 from typing import Any, Text, Dict, List
 import random
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 from rasa_sdk.events import (
     SlotSet,
     EventType,
@@ -21,8 +22,6 @@ from rasa_sdk.events import (
     FollowupAction,
     UserUtteranceReverted,
 )
-
-account_balance = 1_000_000
 
 projects_list = {
     "project_1": {
@@ -77,6 +76,7 @@ class ActionPayProject(Action):
         # }
         pass
 
+
 class ActionShowBalance(Action):
     """Shows the balance of account"""
 
@@ -84,28 +84,77 @@ class ActionShowBalance(Action):
         """Unique identifier of the action"""
         return "action_show_balance"
 
-    async def run(
+    def run(
             self, dispatcher: CollectingDispatcher,
             tracker: Tracker, domain: Dict
         ) -> List[EventType]:
         """Executes the custom action"""
 
         # show bank account balance
-        account_balance = account_balance
-        # amount = tracker.get_slot("amount_transferred")
+        current_account_balance = tracker.get_slot("account_balance")
         dispatcher.utter_message(
             response="utter_account_balance",
-            init_account_balance=f"{account_balance:,}",
+            account_balance=f"{current_account_balance:,}",
         )
 
         events = []
-        # active_form_name = tracker.active_form.get("name")
-        # if active_form_name:
-        #     # keep the tracker clean for the predictions with form switch stories
-        #     events.append(UserUtteranceReverted())
-        #     # trigger utter_ask_{form}_AA_CONTINUE_FORM, by making it the requested_slot
-        #     events.append(SlotSet("AA_CONTINUE_FORM", None))
-        #     # avoid that bot goes in listen mode after UserUtteranceReverted
-        #     events.append(FollowupAction(active_form_name))
 
         return events
+
+
+class ActionWithdraw(Action):
+    """Withdraw from wallet"""
+
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+        return "action_withdrawal"
+
+    async def run(
+            self, dispatcher: CollectingDispatcher,
+            tracker: Tracker, domain: Dict
+        ) -> List[EventType]:
+        """Executes the custom action"""
+
+        current_account_balance = tracker.get_slot("account_balance")
+
+        withdrawal_amount = int(tracker.get_slot("withdrawal_amount"))
+
+        dispatcher.utter_message(
+            response="utter_withdrawal_amount_validated",
+            withdrawal_amount = withdrawal_amount
+        )
+
+        # update slot account balance and withdrawal amount
+        return [
+            SlotSet("account_balance", current_account_balance - withdrawal_amount),
+            SlotSet("withdrawal_amount", None),
+        ]
+
+
+class ValidateWithdrawalForm(FormValidationAction):
+    """Validates Slots of the withdrawal_form"""
+
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+        return "validate_withdrawal_form"
+
+    async def validate_withdrawal_amount(
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+        ) -> Dict[Text, Any]:
+        """Validate the amount of money to withdraw"""
+
+        account_balance = tracker.get_slot("account_balance")
+
+        withdrawal_amount = int(slot_value)
+
+        if withdrawal_amount > account_balance:
+            dispatcher.utter_message(
+                response="utter_insufficient_balance",
+            )
+            return {"withdrawal_amount": None}
+
+        return {"withdrawal_amount": slot_value}
